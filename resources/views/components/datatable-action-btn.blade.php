3799afc944da64d2e57dcb3c36880613971e08ed
Muhamad Ardalepa <69201192+muhamadArdalepa@users.noreply.github.com>
2023-01-15 09:51:44 +0700

    second commit

--- resources/views/components/datatable-action-btn.blade.php
+++ resources/views/components/datatable-action-btn.blade.php
@@ -3,6 +3,10 @@
         data-target="#addCharModal">
         <i class="fas fa-eye"></i>
     </a>
+    <a href="{{asset('storage/'.$data->file)}}" class="btn btn-primary btn-sm" data-toggle="modal"
+        data-target="#addCharModal">
+        <i class="fas fa-eye"></i>
+    </a>
     <a href="{{route('download.masuk',$data->file)}}" class="btn btn-outline btn-sm" data-toggle="modal"
         data-target="#addCharModal">
         <i class="fa-solid fa-download"></i>
